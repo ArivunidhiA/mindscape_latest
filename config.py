948215a34872afd2cd2ee2a1a319ba425eb4e50f
ArivunidhiA <arivunidhi.a@gmail.com>
2025-04-30 13:03:34 -0400

    Update PostgreSQL SSL configuration to use sslmode=require for Render deployment

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config:
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     if SQLALCHEMY_DATABASE_URI and 'postgresql://' in SQLALCHEMY_DATABASE_URI:
-        SQLALCHEMY_DATABASE_URI += "?sslmode=verify-full"
+        SQLALCHEMY_DATABASE_URI += "?sslmode=require"
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
